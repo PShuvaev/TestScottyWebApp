6954fd37099145b12ee2fe419cdb0dd9072e5c62
ipshuvaev <ipshuvaev@gmail.com>
2014-11-03 12:19:03 +0400

    rename sql.sql to initDB.sql

---
+++ initDB.sql
@@ -0,0 +1,13 @@
+create database ScottyTest CHARACTER SET utf8 COLLATE utf8_bin;
+use ScottyTest;
+create table Items (id int not null auto_increment, name varchar(300), price decimal (10, 2), count decimal (10, 2), primary key (id));
+insert into Items (name, count, price) values ('Cheese', 100, 290);
+insert into Items (name, count, price) values ('Cheese', 100, 290);
+insert into Items (name, count, price) values ('Cheese', 100, 290);
+insert into Items (name, count, price) values ('Cheese', 100, 290);
+insert into Items (name, count, price) values ('Cheese', 100, 290);
+insert into Items (name, count, price) values ('Cheese', 100, 290);
+insert into Items (name, count, price) values ('Cheese', 100, 290);
+insert into Items (name, count, price) values ('Cheese', 100, 290);
+insert into Items (name, count, price) values ('Cheese', 100, 290);
+insert into Items (name, count, price) values ('Cheese', 100, 290);
